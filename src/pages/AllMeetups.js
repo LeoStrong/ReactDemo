402c19953059deebfd09e94bf6e2f338ef2fd0a1
Peng <pl596967@gmail.com>
2022-06-02 19:51:02 +0930

    use map to set up list item

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,40 @@
+const DUMMY_DATA = [
+    {
+        id: "m1",
+        title: 'This is a first meetup',
+        image:'https://v.fastcdn.co/u/f91f856b/60142610-0-slide-1.jpg',
+        address: 'Sydney, AU',
+        description: `Whether you’re interested in real estate and cryptocurrency or entrepreneurship and business development, you can share your expertise or simply grow on Meetup.`,
+    },
+    {
+        id: "m2",
+        title: 'This is the second meetup',
+        image:'https://v.fastcdn.co/u/f91f856b/60142600-0-slide-3.jpg',
+        address: 'Adelaide, AU',
+        description: `Yoga, meditation, outdoor activities, fitness, and sports groups are always in high demand. Create a group and share what you know.`,
+    },
+    {
+        id: "m3",
+        title: 'This is the third meetup',
+        image:'https://v.fastcdn.co/u/f91f856b/60141425-0-slide-4.jpg',
+        address: 'Sydney, AU',
+        description: `Find new friends at any age. Start a group for happy hours, singles, support, dining out, and more!`,
+    },
+]
+
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>
+    return (
+        <section>
+            <h1>All Meetups</h1>
+            <ul>
+            {DUMMY_DATA.map((meetup) => {
+                return <li key={meetup.id}>{meetup.title}</li>
+            })}
+            </ul> 
+        </section>
+    );
 }
 
 export default AllMeetupsPage;
+
+
